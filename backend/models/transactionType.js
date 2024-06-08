dda523562a7619ab5259f28a4f334af78565ed4d
proS1996 <sonipronaldo1996@gmail.com>
2024-06-08 18:35:34 +0530

    feat : frontend initial setup

--- backend/models/transactionType.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const transactionTypeSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  // Other transaction type-related attributes...
-});
-
-module.exports = mongoose.model("TransactionType", transactionTypeSchema);
