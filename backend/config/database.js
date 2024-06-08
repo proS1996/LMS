bb02e51f88256647c3258ee4db3a495d560b23ae
proS1996 <sonipronaldo1996@gmail.com>
2024-06-08 19:01:03 +0530

    fix : dotenv file

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,14 +2,11 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 mongoose.set("strictQuery", true);
 
-dotenv.config({ path: "../banking-app/.env" });
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB connected successfully");
   } catch (err) {
